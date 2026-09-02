66b939d894a05a986ddbb83bbba4684997f5844f
valenk1310@gmail.com <kvurbina@uamv.edu.ni>
2026-09-02 11:21:51 -0600

    Agrega launcher al menu principal

---
+++ MenuPrincipal/src/main/java/ni/edu/uam/menuprincipal/Launcher.java
@@ -0,0 +1,9 @@
+package ni.edu.uam.menuprincipal;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(MenuApplication.class, args);
+    }
+}
