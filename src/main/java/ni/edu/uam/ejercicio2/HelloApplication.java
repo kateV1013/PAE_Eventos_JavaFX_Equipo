4e187c6e9f2fb708e252538de5923953197bd31e
valenk1310@gmail.com <kvurbina@uamv.edu.ni>
2026-09-02 07:13:24 -0600

    Crea vista FXML para recepcion de cafe

--- src/main/java/ni/edu/uam/ejercicio2/HelloApplication.java
+++ src/main/java/ni/edu/uam/ejercicio2/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("cafe-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 620);
         stage.setTitle("Recepcion de cafe");
         stage.setScene(scene);
